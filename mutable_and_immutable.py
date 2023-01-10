b4df5ffb55f91094da6b9e39bee25b6abdbfe7fb
jod35 <jodestrevin@gmail.com>
2023-01-10 14:04:36 +0300

    mutable and immutable

--- mutable_and_immutable.py
+++ mutable_and_immutable.py
@@ -11,3 +11,20 @@ print(age) #prints 53
 print(id(age))#second id
 
 #here we can notice that the values of variables can change including the ids
+
+
+class Man:
+    def __init__(self,name) -> None:
+        self.name = name
+
+man1 = Man(name="JONA")
+
+print(id(man1))
+
+print(id(man1))
+
+print(id(man1.name))
+
+
+man1.name ="Jonathan"
+print(id(man1.name)) #change in the value and change in id
