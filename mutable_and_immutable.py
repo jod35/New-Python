b20cc852678fa8c190790f3b75b6b1bdbf74d57e
jod35 <jodestrevin@gmail.com>
2023-01-10 13:44:28 +0300

    mutable and immutable

---
+++ mutable_and_immutable.py
@@ -0,0 +1,13 @@
+age = 45
+
+print(age) # returns 45
+print(id(age)) #first id
+
+#reassign the value 
+
+age = 53
+
+print(age) #prints 53
+print(id(age))#second id
+
+#here we can notice that the values of variables can change including the ids
