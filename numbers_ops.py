4605b47da2b99157c23f0d60fee7c5e6420a9756
jod35 <jodestrevin@gmail.com>
2023-01-10 14:10:59 +0300

    immutable numbers and arithmetic

---
+++ numbers_ops.py
@@ -0,0 +1,9 @@
+a = 12
+b = 3
+
+print(f"{a} + {b} ={a + b}") #addition
+print(f"{a} - {b} ={a - b}") #subtraction
+print(f"{a} * {b} ={a * b}") #multiplication
+print(f"{a} / {b} ={a / b}") #division
+print(f"{a} // {b} ={a // b}") #integer division
+print(f"{a} % {b} ={a % b}") #modulus
