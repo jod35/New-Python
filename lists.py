ea0f3effbf09ea65debf436a2afb7e3b0f3a8c55
jod35 <jodestrevin@gmail.com>
2023-01-11 13:51:08 +0300

    list creations

---
+++ lists.py
@@ -0,0 +1,16 @@
+empty_list =[]
+print(empty_list)
+
+list_obj=list()
+print(list_obj)
+
+numbers = [1,2,3]
+
+list_comprehension=[x**3 for x in [1,2,3,4,5] if x%2 ==0]
+print(list_comprehension)
+
+list_from_tuple = list((1,2,3,4))
+print(list_from_tuple)
+
+list_from_string = list("yEAH BOY")
+print(list_from_string)
