d3d9006bb65a6805075a4e62dbddb48377c9e415
jod35 <jodestrevin@gmail.com>
2023-01-11 13:16:24 +0300

    string formats

---
+++ string_formats.py
@@ -0,0 +1,12 @@
+greeting_old="Hello %s"
+
+print(greeting_old%"Jonathan")
+
+name = "Jonathan"
+
+greeting_new = f"Hello {name}"
+
+print(greeting_new)
+
+greeting_u="Hello {}".format(name)
+print(greeting_u)
