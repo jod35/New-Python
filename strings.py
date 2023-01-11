bc6e771d3703ee90932e371b82564ca435054bf6
jod35 <jodestrevin@gmail.com>
2023-01-11 13:05:39 +0300

    strings

--- strings.py
+++ strings.py
@@ -27,9 +27,14 @@ print(type(str1.encode("utf-8")))
 print(b"Yo boy")
 
 
-sentence = "The problem is yout think you have a lot of time."
+sentence = "The problem is you think you have a lot of time."
 
 print(sentence[0]) #T
 print(sentence[8]) #l
 
-print(sentence[14:40]) #yout think you have a lot
+print(sentence[14:40]) #you think you have a lot
+
+
+print(sentence[4:])
+
+print(sentence[1:23:3])
