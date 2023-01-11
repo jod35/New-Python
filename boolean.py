1e0627797ba6c7f377ed2eb39eb5e3ea7b0ebeee
jod35 <jodestrevin@gmail.com>
2023-01-11 08:37:05 +0300

    booleans

---
+++ boolean.py
@@ -0,0 +1,28 @@
+#booleans are true or false 
+x = True
+
+print(x) #True
+
+y = False
+
+print(y) #False
+
+
+#integer values of booleans
+
+print(int(x)) #1
+
+print(int(y)) #0
+
+#boolean values of common data types
+print(bool(1)) #True
+
+print(bool(0)) #False
+
+print(bool(-23)) #True
+
+print(bool(1000)) #True
+
+print(True + 1) #2
+
+print(False + 1) #1
