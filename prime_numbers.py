99288e7b3b914307015200d77ddb65fde68214e4
jod35 <jodestrevin@gmail.com>
2023-01-13 08:49:30 +0300

    prime numbers

---
+++ prime_numbers.py
@@ -0,0 +1,16 @@
+primes = []
+
+upto = 100
+
+for n in range(2, upto+1):
+    is_prime = True
+
+    for divisor in range(2,n):
+        if n % divisor == 0:
+            is_prime == False
+            break
+
+    if is_prime:
+        primes.append(n)
+
+print(primes)
---
+++ walrus_operator.py
@@ -0,0 +1,5 @@
+value = 100
+modulus = 3
+
+if remainder:= value % modulus:
+    print(f"Not divisible! The remainder is {remainder}")
