7bf5b21da6ff8c7658aeaaed0292e4598f40e78d
jod35 <jodestrevin@gmail.com>
2023-01-14 03:00:57 +0300

    functions

--- functions.py
+++ functions.py
@@ -20,3 +20,16 @@ def sum_of_list(num_list):
 
 sum_=sum_of_list([1,2,3,4])
 print(sum_)
+
+def max_of_numbers(x,y,z):
+    if (x > y) and (x > z):
+        return x
+
+    elif (y > x) and (y> z):
+        return y
+
+    else:
+        return z
+
+print(max_of_numbers(12,123,4))
+        
