ad244ce9eb917e067daff2e58700ac060c7ea70b
jod35 <jodestrevin@gmail.com>
2023-01-16 07:35:05 +0300

    scopes and namespaces

--- scopes_and_namesspaces.py
+++ scopes_and_namesspaces.py
@@ -8,3 +8,19 @@ test_function()
 test = 4
 
 print("Test in global scope",test)
+
+
+def outer():
+    test = 1
+
+    def inner():
+        test =2
+        print(f"Inner: {test}")
+
+    inner()
+
+    print(f"Outer {test}")
+
+test = 0 
+outer()
+print("Global: ",test)
